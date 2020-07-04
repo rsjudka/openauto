06287c6cfede96da20ba103301be66762376d8ed
Cole Brinsfield <brinsfield.cole@gmail.com>
2020-07-03 17:31:05 -0700

    voice session response isn't a thing

--- src/autoapp/Service/AndroidAutoEntity.cpp
+++ src/autoapp/Service/AndroidAutoEntity.cpp
@@ -229,13 +229,12 @@ void AndroidAutoEntity::onNavigationFocusRequest(const aasdk::proto::messages::N
 
 void AndroidAutoEntity::onVoiceSessionRequest(const aasdk::proto::messages::VoiceSessionRequest& request)
 {
-    OPENAUTO_LOG(info) << "[AndroidAutoEntity] Voice session request, type: " << request.type();
 
-    aasdk::proto::messages::VoiceSessionResponse response;
+    OPENAUTO_LOG(info) << "[AndroidAutoEntity] Voice session request, type: " << (request.type()==1)?("START"):((request.type()==2)?("STOP"):("UNKNOWN"));
+
 
     auto promise = aasdk::channel::SendPromise::defer(strand_);
     promise->then([]() {}, std::bind(&AndroidAutoEntity::onChannelError, this->shared_from_this(), std::placeholders::_1));
-    controlServiceChannel_->sendVoiceSessionResponse(response, std::move(promise));
     controlServiceChannel_->receive(this->shared_from_this());
 }
 
